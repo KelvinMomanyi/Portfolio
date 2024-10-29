ecee862473b3011ea343a3c10c0b2d259b520d6d
KelvinMomanyi <kelvinmata42@gmail.com>
2024-10-29 18:09:50 +0300

    new

--- src/assets/assets/index.js
+++ src/assets/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import hotel from "./Royals.JPG";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,12 +20,12 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import Law from "./Law101.PNG"
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-
+import Yard from './Caryard2.PNG';
 import carrent from "./carrent.png";
 import jobit from "./ecom.png";
 import tripguide from "./tripguide.png";
@@ -59,4 +59,7 @@ export {
   carrent,
   jobit,
   tripguide,
+  hotel,
+  Law,
+  Yard
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,9 @@ import {
     jobit,
     tripguide,
     threejs,
+    hotel,
+    Law,
+    Yard
   } from "../assets/assets";
   
   export const navLinks = [
@@ -42,11 +45,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Fullstack Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Mobile Developer",
       icon: mobile,
     },
     {
@@ -54,7 +57,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Graphic Designer",
       icon: creator,
     },
   ];
@@ -133,25 +136,26 @@ import {
       company_name: "Cloudy Solutions",
       icon: tesla,
       iconBg: "#E6DEDD",
-      date: "July 2021 - Dec 2022",
+      date: "July 2021 - Dec 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Building intuitive, efficient, and scalable mobile applications for both iOS and Android using React Native.",  
+        "Working closely with UX/UI designers, product owners, and backend engineers to ensure seamless integration of features.",
+        "Optimizing mobile applications for maximum speed, responsiveness, and performance across a wide range of devices and screen sizes.",
+        "Debugging and troubleshooting mobile application issues across different environments, devices, and operating systems."
       ],
     },
     {
       title: "Full stack Developer",
-      company_name: "Canin Digital",
+      company_name: "Brandefy Creative Studio",
       icon: meta,
       iconBg: "#E6DEDD",
-      date: "Jan 2022 - Present",
+      date: "March 2024 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Designing, developing, and maintaining robust applications using the MERN stack (MongoDB, Express.js, React.js, Node.js) with a focus on scalability and performance.",
+        "Collaborating closely with frontend and backend teams to create seamless user experiences and effective API integrations.",
+        "Managing database operations, including data modeling, queries, and optimization, to support application functionality and improve efficiency.",
+        "Building and implementing RESTful APIs to support frontend needs and ensure efficient data handling across the application.",
+        "Performing thorough testing and debugging across the stack, ensuring cross-platform functionality and optimal performance."
       ],
     },
   ];
@@ -184,6 +188,31 @@ import {
   ];
   
   const projects = [
+    {
+      name: "Hotel Booking App",
+      description:
+        "A hotel booking system is a software platform designed to streamline the reservation process for hotels, resorts, and similar accommodations. It allows guests to book rooms directly through the hotel's website or app, providing real-time availability, pricing, and secure payment options.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "typescript",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: hotel,
+      source_code_link: "https://github.com/KelvinMomanyi/Finance-Dashboard",
+    },
     {
       name: "Finance Dashboard",
       description:
@@ -232,7 +261,57 @@ import {
         },
       ],
       image: jobit,
-      source_code_link: "https://loquacious-torrone-6047c4.netlify.app/",
+      source_code_link: "https://classyelegance.netlify.app",
+    },
+    {
+      name: "Law Firm Consultancy App",
+      description:
+        "Law firms guide clients through legal processes, advocate on their behalf in court, and provide counsel to help them navigate complex legal issues. Dedicated to upholding justice and protecting clients' rights, law firms strive to achieve favorable outcomes and deliver ethical, effective legal solutions",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "typescript",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: Law,
+      source_code_link: "https://kinyakiokeyo.com/",
+    },
+    {
+      name: "Car Selling App",
+      description:
+        "A car selling app is a digital platform that connects buyers and sellers of new and used vehicles, offering a convenient and efficient way to explore, compare, and purchase cars. With a comprehensive listing of cars from various makes, models, and price ranges, the app allows users to filter by preferences, view high-quality images, and access detailed car specification",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "typescript",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: Yard,
+      source_code_link: "https://riftcars.netlify.app/",
     },
     {
       name: "Travel App",
@@ -254,7 +333,7 @@ import {
       ],
       image: tripguide,
       source_code_link: "https://travel-landing-page-livid.vercel.app/",
-    },
+    }
   ];
   
   export { services, technologies, experiences, testimonials, projects };
